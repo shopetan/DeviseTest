b221901217c50301a5af768dd60be0a7d9d1fedd
shopetan <s.kikuchi1120@gmail.com>
2014-11-09 19:05:32 +0900

    Welcome画面の作成

--- DeviseTest/config/routes.rb
+++ DeviseTest/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  root :to => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
